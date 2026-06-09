a8adc7be5a41b31fc41101d5ca854717f3f507d4
Andre Sirait <andredesirait@gmail.com>
2026-06-09 17:01:23 +0800

    refresh css version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 
 
 <!-- CSS -->
- <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}">
+ <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}v=2">
 
 <!-- =========================================
      HERO SECTION
