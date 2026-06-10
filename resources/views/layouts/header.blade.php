dccc0004c51b344fac4ac6c0630ce3ee5d461a98
Andre Sirait <andredesirait@gmail.com>
2026-06-10 17:45:23 +0800

    update terbaru

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="{{ asset('assets/css/header.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/header.css') }}?v=2">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 <nav class="navbar navbar-expand-lg custom-navbar fixed-top">
