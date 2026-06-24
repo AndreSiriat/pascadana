c6f81905f2529a123868412425facbbd75fcc712
Andre Sirait <andredesirait@gmail.com>
2026-06-24 15:52:30 +0800

    Improve homepage SEO and brand keywords

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,7 +46,7 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 
     <div class="hero-content">
         <span class="hero-subtitle">
-            Menghubungkan Pulau, Menyampaikan Kepercayaan
+            PT Pasca Dana Sundari • PDS Ferry Indonesia
         </span>
 
         <h1>
