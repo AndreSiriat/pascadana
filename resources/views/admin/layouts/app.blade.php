18fa0e219d5cfa66812b8a4022cf16c1b7e228ba
Andre Sirait <andredesirait@gmail.com>
2026-06-24 20:08:48 +0800

    add admin favicon

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,6 +3,9 @@
 
 <head>
     @include('admin.partials.head')
+
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/img/200.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/img/200.png') }}">
 </head>
 
 <body>
